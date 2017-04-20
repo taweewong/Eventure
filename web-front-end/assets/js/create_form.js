1c0e63b08812c6db0ef9ae1e324514a2dcd81780
Taweewong Tocharoen <oat_taweewong@hotmail.com>
2017-04-20 17:21:30 +0700

    Update Create form page

--- web-front-end/assets/js/create_form.js
+++ web-front-end/assets/js/create_form.js
@@ -1,30 +1,60 @@
-var textarea = document.querySelector('textarea');
+var textarea = document.querySelector('#q1');
 textarea.addEventListener('keydown', autosize);
              
 function autosize(){
-  var el = this;
-  setTimeout(function(){
-    el.style.cssText = 'height:auto; padding:0';
-    // for box-sizing other than "content-box" use:
-    // el.style.cssText = '-moz-box-sizing:content-box';
-    el.style.cssText = 'height:' + el.scrollHeight + 'px';
-  },0);
+  	var el = this;
+  	setTimeout(function(){
+    	el.style.cssText = 'height:auto; padding:0';
+    	// for box-sizing other than "content-box" use:
+    	// el.style.cssText = '-moz-box-sizing:content-box';
+    	el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  	},0);
 }
 
-var count = 2;
+// Delete question
+$('.question').on("click", "span", function(event) {
+	$(this).parent().parent().fadeOut(function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
 
-var html = '';
-html += '<div class="question">\n'
-html += '<textarea class="input-question" rows="1" name="answer1">Question' + count + '</textarea>\n'
-html += '<hr align="left">\n'
-html += '</div>\n'
+var count = 1;
 
-$("#add-btn").click(function() {
-	$("form").append(html)
+var html;
+
+$("#add-btn").click(function() {	
+	// reset html variable
 	count += 1;
 	html = '';
 	html += '<div class="question">\n'
-	html += '<textarea class="input-question" rows="1" name="answer1">Question' + count + '</textarea>\n'
+	html += '<h3>Question ' + count + ' <span><i class="fa fa-trash-o" aria-hidden="true"></i></span></h3>\n';
+	html += '<textarea id="q' + count + '" class="input-question" rows="1" name="answer1"></textarea>\n'
 	html += '<hr align="left">\n'
 	html += '</div>\n'
+
+	// add html
+	$("form").append(html);
+	
+	// Delete question
+	$('.question').on("click", "span", function(event) {
+		$(this).parent().parent().fadeOut(function() {
+			$(this).remove();
+		});
+		event.stopPropagation();
+	});
+	
+	// add listener
+	textarea = document.querySelector('#q' + count);
+	textarea.addEventListener('keydown', autosize);
+
+
+});
+
+// Delete question
+$('#q1').on("click", "span", function(event) {
+	$(this).parent().parent().fadeOut(function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
